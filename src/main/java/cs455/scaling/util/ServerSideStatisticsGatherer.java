5e392fe24b7a0fd89692e29643a81e553accadc3
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2020-02-27 15:29:22 -0700

    Added methods for calculating server side statistics, added console print statement for printing statistics every 20 seconds

--- src/main/java/cs455/scaling/util/ServerSideStatisticsGatherer.java
+++ src/main/java/cs455/scaling/util/ServerSideStatisticsGatherer.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.nio.channels.SelectionKey;
+import java.sql.Timestamp;
 import java.util.*;
 
 public class ServerSideStatisticsGatherer {
@@ -39,6 +40,13 @@ public class ServerSideStatisticsGatherer {
                     statisticsGatherer.resetThroughPuts();
                 }
                 LOG.debug("Through put list from last 20 seconds: " + Arrays.asList(currentThroughPutList));
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                double totalMessagesPerSecond = calculateTotalThroughput(currentThroughPutList) / 20.0;
+                int totalActiveClients = calculateTotalActiveClients(currentThroughPutList);
+                double throughputMeanPerSecond = calculateMean(currentThroughPutList) / 20.0;
+                double throughputStandardDeviation = calculatePerSecondStandardDeviation(currentThroughPutList);
+                System.out.printf("%s Server Throughput: %f/s, Active Client Connections: %d, Mean Per-client Throughput: %f/s, Std. Dev. Of Per-client Throughput: %f/s\n",
+                        timestamp, totalMessagesPerSecond, totalActiveClients, throughputMeanPerSecond, throughputStandardDeviation);
             }
         };
 
@@ -60,5 +68,35 @@ public class ServerSideStatisticsGatherer {
             SelectionKey key = entry.getKey();
             individualClientThroughPuts.put(key, 0);
         }
+    }
+
+    private int calculateTotalThroughput(Map<SelectionKey, Integer> throughputList) {
+        int totalThroughput = 0;
+        for (Map.Entry<SelectionKey, Integer> entry : throughputList.entrySet()) {
+            totalThroughput += entry.getValue();
+        }
+        return totalThroughput;
+    }
+
+    private int calculateTotalActiveClients(Map<SelectionKey, Integer> throughputList) {
+        return throughputList.size();
+    }
+
+    private double calculateMean(Map<SelectionKey, Integer> throughputList) {
+        double totalThroughput = calculateTotalThroughput(throughputList);
+        double totalActiveClients = calculateTotalActiveClients(throughputList);
+        return totalThroughput / totalActiveClients;
+    }
+
+    private double calculatePerSecondStandardDeviation(Map<SelectionKey, Integer> throughputList) {
+        double throughputMean = calculateMean(throughputList);
+        int totalActiveClients = calculateTotalActiveClients(throughputList);
+        double variance = 0;
+        for (Map.Entry<SelectionKey, Integer> entry : throughputList.entrySet()) {
+            double squaredDifference = Math.pow(entry.getValue() - throughputMean, 2.0) / 20.0;
+            variance += (squaredDifference / totalActiveClients);
+        }
+        double standardDeviation = Math.sqrt(variance);
+        return standardDeviation;
     }
 }
